d6b9a31154439b401d8d469980268476fa5b582f
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-05-06 13:04:36 +0200

    throttle store subscription

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
+import throttle from 'lodash/throttle';
+
 import rootReducer from '../reducers';
 import { loadState, saveState } from '../utils/localStorage';
 
@@ -14,13 +16,16 @@ const configureStore = () => {
     composeWithDevTools(applyMiddleware(thunk)),
   );
 
-  store.subscribe(() => {
-    saveState({
-      user: store.getState().user,
-      trip: store.getState().trip,
-      routing: store.getState().routing,
-    });
-  });
+  store.subscribe(
+    throttle(() => {
+      saveState({
+        user: store.getState().user,
+        trip: store.getState().trip,
+        routing: store.getState().routing,
+      });
+    }),
+    1000,
+  );
 
   return store;
 };
