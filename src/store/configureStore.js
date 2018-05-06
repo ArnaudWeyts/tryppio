2b8b593452f37d53d1834d7a5ee1b4dc1df36f9d
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-05-06 12:28:28 +0200

    save user to localStorage

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,12 +3,20 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+import { loadState, saveState } from '../utils/localStorage';
+
+const persistedState = loadState();
 
 const configureStore = () => {
   const store = createStore(
     rootReducer,
+    persistedState,
     composeWithDevTools(applyMiddleware(thunk)),
   );
+
+  store.subscribe(() => {
+    saveState({ user: store.getState().user });
+  });
 
   return store;
 };
